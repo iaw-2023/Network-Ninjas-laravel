aa75f8c0d29a019ab2be941e1e7c085889fea644
giulianogiannotti <71414041+giulianogiannotti@users.noreply.github.com>
2023-06-25 11:54:53 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['auth:sanctum']], function (Request $request) {
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/pedidos/search/{id}', [PedidoController::class, 'searchByClientId']);
 });
 
